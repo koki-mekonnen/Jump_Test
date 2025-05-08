6cbf89b863a1114205d5c16501a86b9152d594b5
Selamawit <ezanawittadele@gmail.com>
2025-05-08 15:22:31 +0300

    feat: configure for deploy

--- config/prod.exs
+++ config/prod.exs
@@ -6,6 +6,7 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :markdown_editor, MarkdownEditorWeb.Endpoint,
+  server: true,
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Configures Swoosh API Client
